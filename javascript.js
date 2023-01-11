dd2b0d10ad6e2d3ad9d1f5ef02bc7f5f2a4569cc
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2023-01-11 15:37:42 +0000

    For any given cell, select all neighbours

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
+//First create the grid
+//Doing this in JS to avoid a huge HTML file
+
 const gridContainer = document.querySelector("#grid-container");
-
-//const gridCells = document.querySelectorAll(".grid-cell");
-
 function createGrid() {
     for (let i = 1; i < 1251; i++) {
         gridCell = document.createElement("div");
@@ -10,5 +10,34 @@ function createGrid() {
         gridContainer.appendChild(gridCell);
     }
 }
-
 createGrid();
+
+//Then
+
+let gridCells = document.querySelectorAll(".grid-cell");
+
+gridCells.forEach(cell => cell.addEventListener("click", gridCellInfo));
+
+function gridCellInfo() {
+    this.classList.add("alive");
+    let cellNum = Number(this.dataset.num);
+    console.log(cellNum);
+    console.log(cellNum % 50);
+
+    let neighborNumbers = [];
+
+    if (cellNum % 50 === 0) {
+        neighborNumbers = ["-1", "49", "50", "-50", "-51"];
+    } else if (cellNum % 50 === 1) {
+        neighborNumbers = ["1", "-49", "50", "-50", "51"];
+    } else {
+        neighborNumbers = ["1", "-1", "49", "-49", "50", "-50", "51", "-51"];
+    }
+
+    neighborNumbers.forEach(function(num) {
+        neighborNum = cellNum + Number(num);
+        let neighbor = document.querySelector(`[data-num="${neighborNum}"]`);
+        if (!neighbor) return;
+        neighbor.classList.add("neighbor");
+    });
+}
